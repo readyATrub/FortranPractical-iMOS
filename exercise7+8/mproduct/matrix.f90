b19504a435e85cc7c74f677ed64688286f357e03
readyATrub <121827682+readyATrub@users.noreply.github.com>
2023-01-03 00:07:54 +0100

    See Description
    Merged makea and makeb to single subroutine makemarices, replaced do loops for the trace with a subroutine doing that and formatted output

--- exercise7+8/mproduct/matrix.f90
+++ exercise7+8/mproduct/matrix.f90
@@ -1,41 +1,26 @@
 MODULE matrix
     PRIVATE
-    PUBLIC :: makea, makeb, multiplicate, dummy
+    PUBLIC :: makematrices, multiplicate, trace, dummy
 
     contains
-    FUNCTION makea (n) result(a)
+    SUBROUTINE makematrices (a,b,n)
         IMPLICIT NONE
         INTEGER, INTENT(IN) :: n
         INTEGER :: i,j
         REAL :: nr
         REAL, PARAMETER :: pi= acos(-1.0)
-        REAL, DIMENSION(1:n,1:n):: a
+        REAL, DIMENSION(:,:), ALLOCATABLE:: a,b
 
+        ALLOCATE(a(1:n,1:n),b(1:n,1:n))
         nr = n
         
         DO i=1, n 
             DO j=1, n 
-                a(i,j)= sin(2*pi*i*j/nr)    
+                a(i,j)= sin(2*pi*i*j/nr) 
+                b(i,j)= cos(2*pi*i*j/nr)
              END DO
         END DO
-    END FUNCTION makea
-
-    FUNCTION makeb (n) result(b)
-        IMPLICIT NONE
-        INTEGER, INTENT(IN) :: n
-        INTEGER :: i,j
-        REAL :: nr
-        REAL, PARAMETER :: pi= acos(-1.0)
-        REAL, DIMENSION(1:n,1:n):: b
-
-        nr = n
-        
-        DO i=1, n 
-            DO j=1, n 
-                b(i,j)= cos(2*pi*i*j/nr)
-            END DO
-        END DO
-    END FUNCTION makeb
+    END SUBROUTINE makematrices
 
     FUNCTION multiplicate (a,b,n) result (c)
         IMPLICIT NONE
@@ -52,6 +37,20 @@ MODULE matrix
             END DO    
         END DO
     END FUNCTION multiplicate
+
+    FUNCTION trace(a,n) result (tr)
+        IMPLICIT NONE
+        INTEGER, INTENT(IN) :: n
+        INTEGER :: i
+        REAL :: tr
+        REAL, DIMENSION(1:n,1:n), INTENT(IN):: a
+
+        tr = 0
+        DO i=1, n
+            tr = tr + a(i,i)
+        END DO
+    
+    END FUNCTION trace
 
     SUBROUTINE dummy (a,b,n)
         IMPLICIT NONE
