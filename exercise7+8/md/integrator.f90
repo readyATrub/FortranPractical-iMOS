ade5e291bd0ee3eec227b429d4c342881c7a210a
readyATrub <121827682+readyATrub@users.noreply.github.com>
2023-01-18 20:13:03 +0100

    Repaired the 2nd and 3rd euler expansion

--- exercise7+8/md/integrator.f90
+++ exercise7+8/md/integrator.f90
@@ -21,8 +21,8 @@ MODULE integrator
         REAL, INTENT(IN) :: x0, v0, dt, m
         REAL :: x, v
 
-        x = x0 + v0*dt + 0.5 * (f(x0)/m) * dt**2.0
-        v = v0 + (f(x0)/m) * dt + (tdf(x0) * v0)/m * dt**2.0
+        x = x0 + v0*dt + 0.5 * (f(x0)/m) * (dt**2.0)
+        v = v0 + (f(x0)/m) * dt + 0.5 * ((tdf(x0) * v0)/m) * (dt**2.0)
 
     END SUBROUTINE euler2
     
@@ -31,9 +31,9 @@ MODULE integrator
         REAL, INTENT(IN) :: x0, v0, dt, m
         REAL :: x, v
 
-        x = x0 + v0*dt + 0.5 * (f(x0)/m) * dt**2.0 + (tdf(x0) * v0)/(m*6.0) * dt**3.0
-        v = v0 + (f(x0)/m) * dt + 0.5 * (tdf(x0) * v0)/m * dt**2 + &
-        ((tdf2(x0) * v0**2)/m + (tdf(x0) * (f(x0)/m))/(m*6.0)) * dt**3.0
+        x = x0 + v0*dt + 0.5 * (f(x0)/m) * dt**2.0 + (1.0/6.0) * (tdf(x0) * v0)/(m) * dt**3.0
+        v = v0 + (f(x0)/m) * dt + 0.5 * (tdf(x0) * v0)/m * dt**2 +  (1.0/6.0)* &
+        ((tdf2(x0) * v0**2)/m + (tdf(x0) * (f(x0)/m))/(m)) * dt**3.0
 
     END SUBROUTINE euler3
 
